d8eb122720c7d7e1fe7ecc375b142712bb0636d8
dani <dani.zaghian@gmail.com>
2019-10-25 13:31:16 -0700

    adds vue.js frontend

--- config/routes.rb
+++ config/routes.rb
@@ -21,4 +21,6 @@ Rails.application.routes.draw do
     post "/sessions" => "sessions#create"
   end
 
+  get "/*path" => proc { [200, {}, [ActionView::Base.new.render(file: 'public/index.html')]] }
+
 end
